32008eeebf77f99a15b9063f60cd56328aa10b84
Arun Patra <arunkpatra@gmail.com>
2021-01-07 14:18:01 +0530

    Added Travis CI integration

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -3,3 +3,4 @@ set -ex
 wget https://github.com/protocolbuffers/protobuf/releases/download/v3.14.0/protoc-3.14.0-linux-x86_64.zip
 sudo unzip -o protoc-3.14.0-linux-x86_64.zip -d /usr/local bin/protoc
 sudo unzip -o protoc-3.14.0-linux-x86_64.zip -d /usr/local 'include/*'
+ls -al /usr/local/bin/protoc
