d79f45d359b1eeb7e496d544fd37aec2143602c3
Arun Patra <arunkpatra@gmail.com>
2021-01-11 13:14:33 +0530

    Travis CI integration.

--- third_party/protoc-gen.sh
+++ third_party/protoc-gen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 
-protoc-gen-go
+sudo protoc-gen-go
 protoc --proto_path=api/proto/v1 --proto_path=third_party --go_out=plugins=grpc:pkg/api/v1 todo-service.proto
 protoc --proto_path=api/proto/v1 --proto_path=third_party --grpc-gateway_out=logtostderr=true:pkg/api/v1 todo-service.proto
 protoc --proto_path=api/proto/v1 --proto_path=third_party --swagger_out=logtostderr=true:api/swagger/v1 todo-service.proto
