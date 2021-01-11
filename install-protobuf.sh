ff57c6fa5287683c636eef049a0a3c2c269e3a13
Arun Patra <arunkpatra@gmail.com>
2021-01-11 11:51:40 +0530

    Travis CI integration.

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -ex
+#set -ex
 wget https://github.com/protocolbuffers/protobuf/releases/download/v3.14.0/protoc-3.14.0-linux-x86_64.zip
 sudo unzip -o protoc-3.14.0-linux-x86_64.zip -d /usr/local bin/protoc
 sudo unzip -o protoc-3.14.0-linux-x86_64.zip -d /usr/local 'include/*'
--- third_party/protoc-gen.sh
+++ third_party/protoc-gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -ex
+#set -ex
 
 sudo protoc --proto_path=api/proto/v1 --proto_path=third_party --go_out=plugins=grpc:pkg/api/v1 todo-service.proto
 sudo protoc --proto_path=api/proto/v1 --proto_path=third_party --grpc-gateway_out=logtostderr=true:pkg/api/v1 todo-service.proto
